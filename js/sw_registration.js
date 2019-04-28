35c590ae484588e0dab089498e43e58574da0b97
Nicola Centrone <nicola.centrone.it@gmail.com>
2019-04-28 10:45:11 +0200

    feat: Add service worker registration

---
+++ js/sw_registration.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/js/sw.js').then(function() {
+    console.log('Service worker registration worked');
+  }).catch(function(error){
+    console.log('Service worker registration failed:' + error);
+  });
+}
