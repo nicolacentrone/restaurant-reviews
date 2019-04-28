c29353223e1feccb24b4ade86d7e0ef15c24c2f9
Nicola Centrone <nicola.centrone.it@gmail.com>
2019-04-28 17:37:19 +0200

    fix: Move sw.js into the root

--- sw_registration.js
+++ sw_registration.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('js/sw.js').then(function() {
+  navigator.serviceWorker.register('sw.js').then(function() {
     console.log('Service worker registration worked');
   }).catch(function(error){
     console.log('Service worker registration failed:' + error);
